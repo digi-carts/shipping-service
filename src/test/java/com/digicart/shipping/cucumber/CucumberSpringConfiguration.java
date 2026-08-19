aab41cf5e271f8a448117c69fa944c6480d3de2b
Ananthu PM <ananthupmadhu@gmail.com>
2026-08-20 00:49:34 +0530

    fix: run Cucumber component tests against in-memory H2
    CI mvn test now boots Spring with a test profile so JPA services do not need Postgres.

--- src/test/java/com/digicart/shipping/cucumber/CucumberSpringConfiguration.java
+++ src/test/java/com/digicart/shipping/cucumber/CucumberSpringConfiguration.java
@@ -2,20 +2,12 @@ package com.digicart.shipping.cucumber;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import com.digicart.shipping.exception.GlobalExceptionHandler;
-import com.digicart.shipping.controller.HealthController;
-import com.digicart.shipping.controller.ShipmentController;
-import com.digicart.shipping.service.ShipmentService;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
-@WebMvcTest(controllers = { HealthController.class, ShipmentController.class })
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc(addFilters = false)
-@Import(GlobalExceptionHandler.class)
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
-    @MockBean
-    ShipmentService shipmentService;
-
 }
